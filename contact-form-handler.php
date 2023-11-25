be513735973fc32c16b0287751b52424cd1ddec4
giasolanabrown <148883246+giasolanabrown@users.noreply.github.com>
2023-11-24 20:08:15 -0500

    Create contact-form-handler.php

---
+++ contact-form-handler.php
@@ -0,0 +1,42 @@
+<?php 
+$errors = '';
+$myemail = 'hi@giasolana.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$message = $_POST['message']; 
+
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
